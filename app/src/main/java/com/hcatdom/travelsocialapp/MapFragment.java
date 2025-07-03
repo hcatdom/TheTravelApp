2be85413456b4580ad3c513f7776d101ec6b4503
Your Name <you@example.com>
2025-07-03 17:17:42 +0200

    Pines puestos más o menos

--- app/src/main/java/com/hcatdom/travelsocialapp/MapFragment.java
+++ app/src/main/java/com/hcatdom/travelsocialapp/MapFragment.java
@@ -3,7 +3,8 @@ package com.hcatdom.travelsocialapp;
 import android.graphics.Matrix;
 import android.graphics.RectF;
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -17,16 +18,41 @@ import io.getstream.photoview.PhotoView;
 public class MapFragment extends Fragment {
 
     public MapFragment() {
-        // Required empty public constructor
-        PhotoView photoView = findViewById(R.id.photoView);
+        // Constructor vacío requerido
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        // Inflar el layout del fragmento
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Aquí ya tienes acceso a la vista inflada
+        PhotoView photoView = view.findViewById(R.id.photoView);
         ImageView pin1 = view.findViewById(R.id.pin1);
+        ImageView pin2 = view.findViewById(R.id.pin2);
+        ImageView pin3 = view.findViewById(R.id.pin3);
+        ImageView pin4 = view.findViewById(R.id.pin4);
 
+        // Coordenadas del pin en la imagen original (por ejemplo, 3000x2000 px)
+        final float originalX1 = 1500f;
+        final float originalY1 = 800f;
+        final float originalX2 = 2350f;
+        final float originalY2 = 2800f;
+        final float originalX3 = 5800f;
+        final float originalY3 = 4000f;
+        final float originalX4 = 4800f;
+        final float originalY4 = 1200f;
 
-// Coordenadas del pin en la imagen original (por ejemplo, 3000x2000)
-        final float originalX = 1500f;
-        final float originalY = 800f;
-
-// Escucha de cambios de transformación
+        // Escucha los cambios de zoom y movimiento
         photoView.setOnMatrixChangeListener(new OnMatrixChangedListener() {
             @Override
             public void onMatrixChanged(RectF rect) {
@@ -38,20 +64,26 @@ public class MapFragment extends Fragment {
                 float transX = matrixValues[Matrix.MTRANS_X];
                 float transY = matrixValues[Matrix.MTRANS_Y];
 
-                // Calculamos dónde debería estar el pin en pantalla
-                float pinX = originalX * scale + transX;
-                float pinY = originalY * scale + transY;
+                // Calcula posición del pin en pantalla
+                float pinX1 = originalX1 * scale + transX;
+                float pinY1 = originalY1 * scale + transY;
+                float pinX2 = originalX2 * scale + transX;
+                float pinY2 = originalY2 * scale + transY;
+                float pinX3 = originalX3 * scale + transX;
+                float pinY3 = originalY3 * scale + transY;
+                float pinX4 = originalX4 * scale + transX;
+                float pinY4 = originalY4 * scale + transY;
 
-                // Colocamos el pin (ajustando para centrarlo)
-                pin1.setX(pinX - pin1.getWidth() / 2f);
-                pin1.setY(pinY - pin1.getHeight() / 2f);
+                // Coloca el pin (centrado)
+                pin1.setX(pinX1 - pin1.getWidth() / 2f);
+                pin1.setY(pinY1 - pin1.getHeight() / 2f);
+                pin2.setX(pinX2 - pin2.getWidth() / 2f);
+                pin2.setY(pinY2 - pin2.getHeight() / 2f);
+                pin3.setX(pinX3 - pin3.getWidth() / 2f);
+                pin3.setY(pinY3 - pin3.getHeight() / 2f);
+                pin4.setX(pinX4 - pin4.getWidth() / 2f);
+                pin4.setY(pinY4 - pin4.getHeight() / 2f);
             }
         });
-
-//    private <__TMP__> __TMP__ findviewbyid() {
-//    }
-//
-//    private <__TMP__> __TMP__ findviewbyid() {
     }
-
- }
+}
